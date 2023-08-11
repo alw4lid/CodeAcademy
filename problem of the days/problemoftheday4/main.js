7592cf28592748d15b48f34c2f66fe0edcea184f
alw4lid <140022449+alw4lid@users.noreply.github.com>
2023-08-11 18:52:37 +0400

    Update main.js
    Modified my answer due to the way of the question written >.>

--- problem of the days/problemoftheday4/main.js
+++ problem of the days/problemoftheday4/main.js
@@ -1,8 +1,8 @@
 const Validate = (uname) => {
-  if (uname.length <= 9) {
-    console.log("User is a Boy");
-  } else {
+  if (uname.length %2 == 0) {
     console.log("User is a Girl");
+  } else {
+    console.log("User is a Boy");
   }
 };
 
